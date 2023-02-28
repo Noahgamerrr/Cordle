95e5030708b01f3e25ac959fb93917873f37062d
Noahgamerrr <noah.arsic@gmx.at>
2023-02-28 12:36:46 +0100

    Guesses can now be made

--- main.c
+++ main.c
@@ -5,6 +5,11 @@
 #define MAP_LENGTH 5
 #define FILE_LENGTH 15918
 #define WORD_LENGTH 6
+#define GUESS_LENGTH 50
+#define ATTEMPS 6
+
+char **attempts;
+int tries = 0;
 
 char* read_word() {
     FILE* ptr;
@@ -12,7 +17,7 @@ char* read_word() {
     srand(time(NULL));
     int r = rand() % FILE_LENGTH;
     int i = 0;
-    ptr = fopen("..\\words.txt", "r");
+    ptr = fopen(".\\words.txt", "r");
     do {
         fgets(word, WORD_LENGTH + 1, ptr);
         i++;
@@ -21,10 +26,9 @@ char* read_word() {
     return word;
 }
 
-void init_map(Map* map) {
-    char *word = "hallo";
-    printf("%s\n", word);
-    for (size_t i = 0; i < strlen(word); i++) {
+char* init_map(Map* map) {
+    char *word = read_word();
+    for (size_t i = 0; i < WORD_LENGTH - 1; i++) {
         char *current_char = malloc(sizeof(char));
         int *value = malloc(sizeof(int));
         *current_char = word[i];
@@ -32,16 +36,54 @@ void init_map(Map* map) {
         *value = (*(int*)mapgetordefault(map, current_char, value)) + 1;
         mapput(map, current_char, value);
     }
-    free(word);
+    return word;
+}
+
+void print_attempts(Map* map) {
+    for (int i = 0; i < ATTEMPS; i++) {
+        for (int j = 0; j < WORD_LENGTH - 1; j++) {
+            printf("[");
+            if (tries > i) printf("%c", attempts[i][j]);
+            else printf(" ");
+            printf("]");
+        }
+        printf("\n");
+    }
+}
+
+void run_game(Map *map) {
+    static int allocated = 0;
+    if (!allocated) {
+        attempts = malloc(ATTEMPS * sizeof(char*));
+        allocated = 1;
+    }
+    while (tries <= ATTEMPS) {
+        int attempted = 0;
+        char* attempt;
+        if (!attempted) {
+            attempt = malloc(GUESS_LENGTH * sizeof(char));
+            attempted = 1;
+        }
+        printf("\e[1;1H\e[2J");
+        print_attempts(map);
+        if (tries != ATTEMPS) {
+            printf("Enter your next guess: ");
+            gets(attempt);
+            while (strlen(attempt) != WORD_LENGTH - 1) {
+                printf("Invalid length of word. Enter your next guess: ");
+                gets(attempt);
+            }
+        } else printf("Thanks for playing");
+        attempts[tries] = attempt;
+        tries++;
+    }
 }
 
 int main() {
     Map *map = mapcreate(CHARACTER, INTEGER);
     init_map(map);
-    char* test = malloc(sizeof(char));
-    *test = 'a';
-    printf("%d", *(int*)mapget(map, test));
-    free(test);
+    printf("\e[1;1H\e[2J");
+    run_game(map);
     mapfree(map);
     return 0;
 }
