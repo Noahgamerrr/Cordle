37ee48219994c6fc1039857c52bf9e3ae80a9b25
Noahgamerrr <noah.arsic@gmx.at>
2023-03-04 20:32:45 +0100

    Added comments to the game

--- main.c
+++ main.c
@@ -27,11 +27,17 @@ int lettercolour[ALPHABET_LENGTH];
 int tries = 0;
 int haswon = 0;
 
+/*
+    Clears the screen
+*/
 void cls() {
     if (os) system("clear");
     else system("cls");
 }
 
+/*
+    Frees all the attempts that have been made
+*/
 void freeattempts() {
     for (int i = ATTEMPS - 1; i >= 0; i--) {
         free(attempts[i]);
@@ -39,22 +45,37 @@ void freeattempts() {
     }
 }
 
+/*
+    Changes the font-colour to yellow
+*/
 void yellow() {
     printf("\033[0;33m");
 }
 
+/*
+    Changes the font-colour to green
+*/
 void green() {
     printf("\033[0;32m");
 }
 
+/*
+    Changes the font-colour to white
+*/
 void white() {
     printf("\033[0;37m");
 }
 
+/*
+    Changes the font-colour to bold white
+*/
 void bold_white() {
     printf("\033[1;37m");
 }
 
+/*
+    Reads the word to be guessed from the file
+*/
 void read_word() {
     FILE* ptr;
     word = malloc(WORD_LENGTH * sizeof(char));
@@ -69,6 +90,11 @@ void read_word() {
     fclose(ptr);
 }
 
+/* 
+    Looks if the guess is a valid word or not
+    @param guess The guess made by the user
+    @returns 1 if the guess is a valid word, else 0
+*/
 int isguessvalid(char* guess) {
     FILE* ptr;
     ptr = fopen(wordlist, "r");
@@ -89,6 +115,10 @@ int isguessvalid(char* guess) {
     return valid;
 }
 
+/*
+    Initializes the map which contains information about
+    how often a letter appears in the word
+*/
 void init_wordmap() {
     for (size_t i = 0; i < WORD_LENGTH - 1; i++) {
         char *current_char = malloc(sizeof(char));
@@ -100,6 +130,10 @@ void init_wordmap() {
     }
 }
 
+/*
+    Changes the colour on the console for every letter on the keyboard
+    @param idx The index
+*/
 void draw_colour(int idx) {
     switch (lettercolour[idx]) {
         case 1:
@@ -117,6 +151,11 @@ void draw_colour(int idx) {
     }
 }
 
+/*
+    Returns the place of the letter in the QWERTY-order
+    @param letter The letter
+    @returns The position of the letter in QWERTY-order
+*/
 int get_qwerty_letter(char letter) {
     for (int i = 0; i < ALPHABET_LENGTH; i++) {
         if (letter == lettermap[i]) return i;
@@ -124,6 +163,9 @@ int get_qwerty_letter(char letter) {
     return -1;
 }
 
+/*
+    Initializes the lettermap
+*/
 void init_lettermap() {
     lettermap[0] = 'q';
     lettermap[1] = 'w';
@@ -153,10 +195,16 @@ void init_lettermap() {
     lettermap[25] = 'm';
 }
 
+/*
+    Clears any information about the letters on the keyboard
+*/
 void clearcolours() {
     for (int i = 0; i < ALPHABET_LENGTH; i++) lettercolour[i] = 0;
 }
 
+/*
+    Prints the keyboard to the screen;
+*/
 void print_keyboard() {
     for (int i = 0; i < ALPHABET_LENGTH; i++) {
         draw_colour(i);
@@ -168,10 +216,22 @@ void print_keyboard() {
     white();
 }
 
+/*
+    Finds the bigger values of two integers
+    @param a The first integer
+    @param b The second integer
+    @returns The bigger value
+*/
 int max(int a, int b) {
     return a > b ? a : b;
 }
 
+/*
+    Increases the value of a letter inside the map by one
+    @param i The index for attempts
+    @param j The index for the letter inside attempts[i]
+    @param map The map
+*/
 void increasevalue(int i, int j, Map *map) {
     int* value = malloc(sizeof(int));
     *value = 0;
@@ -179,6 +239,10 @@ void increasevalue(int i, int j, Map *map) {
     mapput(map, &attempts[i][j], value);
 }
 
+/*
+    Fills the guessmap with all the "green" values
+    @param i The amount of guesses that already have been made
+*/
 void prefillguessmap(int i) {
     if (tries > i) {
         for (int j = 0; j < WORD_LENGTH - 1; j++) {
@@ -190,6 +254,9 @@ void prefillguessmap(int i) {
     }
 }
 
+/*
+    Prints out all the attempts that have been made
+*/
 void print_attempts() {
     for (int i = 0; i < ATTEMPS; i++) {
         mapvalueclear(guessmap);
@@ -225,6 +292,9 @@ void print_attempts() {
     }
 }
 
+/*
+    Main-function for handling the game
+*/
 void run_game() {
     while (tries <= ATTEMPS && !haswon) {
         int attempted = 0;
@@ -251,6 +321,9 @@ void run_game() {
     }
 }
 
+/*
+    Prints out the menue
+*/
 int menu() {
     int choice = 0;
     printf("/----------Cordle----------\\\n");
@@ -269,6 +342,9 @@ int menu() {
     return choice;
 }
 
+/*
+    Resets the game after it has been finished
+*/
 void resetgame() {
     freeattempts();
     mapdeepclear(wordmap);
